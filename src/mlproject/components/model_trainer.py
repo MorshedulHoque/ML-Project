c3b98adbb5629037146f1934401368579ba2cc19
unknown <asm21612@gmail.com>
2023-10-14 10:11:11 +0600

    Model trainer added.

--- src/mlproject/components/model_trainer.py
+++ src/mlproject/components/model_trainer.py
@@ -21,6 +21,7 @@ import warnings
 
 from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
+from src.mlproject.utils import save_object,evaluate_models
 
 
 
@@ -43,16 +44,13 @@ class ModelTrainer:
                 test_array[:,-1]
             )
             models = {
-                "Linear Regression": LinearRegression(),
-                "Lasso": Lasso(),
-                "Ridge": Ridge(),
-                "K-Neighbors Regressor": KNeighborsRegressor(),
+                "Random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
-                "Random Forest Regressor": RandomForestRegressor(),
-                "XGBRegressor": XGBRegressor(), 
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "Linear Regression": LinearRegression(),
+                "XGBRegressor": XGBRegressor(),
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
-                "Gradient Boosting": GradientBoostingRegressor()
             }
 
 
@@ -93,6 +91,35 @@ class ModelTrainer:
                 }
                 
             }
+
+            model_report:dict = evaluate_models(X_train, y_train, X_test, y_test, models, params)
+
+            ## To get best model score from dict
+            best_model_score = max(sorted(model_report.values()))
+
+            ## To get best model name from dict
+
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            best_model = models[best_model_name]
+
+            if best_model_score<0.6:
+                raise CustomException("No best model found")
+            logging.info(f"Best found model on both training and testing dataset")
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            predicted=best_model.predict(X_test)
+
+            r2_square = r2_score(y_test, predicted)
+            return r2_square
+
+
+
 
         except Exception as e:
             raise CustomException(e,sys)
